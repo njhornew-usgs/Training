3808c81bfd1b28cbfeb24b42e59b751a09704f41
Nancy <njhornew@usgs.gov>
2016-02-05 09:58:42 -0700

    more changes

--- do_stats.sh
+++ do_stats.sh
@@ -1,8 +1,9 @@
 #Runs goostats on list of files
 #Enter files on command line (AB)
+#Added curly brackets
 #
 for datafile in *[$@].txt
 do
 	echo ${datafile}
-	bash goostats $datafile stats-$datafile
+	bash goostats ${datafile} stats-${datafile}
 done
